d566f31eb883a16b4c66ee23423ce6ebc8cd2842
reuth <jhosephvillalba@gmail.com>
2022-12-13 14:04:22 -0500

    second commit

--- api/config/mongoose.js
+++ api/config/mongoose.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose"); 
 require("dotenv").config(); 
 
-const uri = process.env.BD_URI
 
-// console.log(uri)
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.6p5r94x.mongodb.net/${process.env.NAME_DB}?retryWrites=true&w=majority`
+
 const conn = () => {
     return mongoose.connect(uri, {
         useNewUrlParser: true,
