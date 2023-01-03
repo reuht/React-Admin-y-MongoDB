a7feaa8bfa001b66c29e804a23453a67d94cbaf3
reuth <jhosephvillalba@gmail.com>
2023-01-03 13:07:07 -0500

    cambio

--- api/config/mongoose.js
+++ api/config/mongoose.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 
 
 
-// const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.6p5r94x.mongodb.net/${process.env.NAME_DB}?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.6p5r94x.mongodb.net/${process.env.NAME_DB}?retryWrites=true&w=majority`
 
 const conn = () => {
-    return mongoose.connect("mongodb://mongo:O5lR1T97VFWWo75zjrxM@containers-us-west-173.railway.app:7260", {
+    return mongoose.connect(uri, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       });
